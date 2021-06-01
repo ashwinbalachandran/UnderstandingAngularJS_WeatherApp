c483f3b80e1cc11c5c78c25a4eb6aa08c28066ab
Ashwin Balachandran <ash20win@gmail.com>
2021-05-31 18:23:35 -0700

    Shit with scaling but async is faking me out

--- Starter/controllers.js
+++ Starter/controllers.js
@@ -13,13 +13,15 @@ weatherApp.controller('homeController', ['$scope', '$location', 'cityService', f
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', 'weatherService', function($scope, $resource, $routeParams, cityService, weatherService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
     
     $scope.city = cityService.city;
     
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     
     $scope.convertToFahrenheit = function(degK) {
         
